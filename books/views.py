ef269f1c4340e1493126b66029aeb4ab2ea9e7ad
Lauren <lauren.main28@gmail.com>
2023-06-14 19:40:47 -0700

    adds working books list and books detail

--- books/views.py
+++ books/views.py
@@ -1,9 +1,14 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Book
 
 class BookListView(ListView):
     template_name = "book_list.html"
     model = Book
     context_object_name = "books"
+
+
+class BookDetailView(DetailView):
+    template_name = "book_detail.html"
+    model = Book
 
 
